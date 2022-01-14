e436b952bc1c0ca83654ca5c3cf0ffefc8cc6966
ArcadioGM <arcadiogm.com@gmail.com>
2022-01-14 20:26:06 +0100

    Update 3_deploy_contracts.js

--- migrations/3_deploy_contracts.js
+++ migrations/3_deploy_contracts.js
@@ -1,31 +1,24 @@
-// Lighthouse for random number
+// for random number
 const Lighthouse = artifacts.require("Lighthouse");
-// Fojini Exchange Smart Contract 
 const Exchange   = artifacts.require("Exchange");
 
-//web3 and accounts are available in the migrations deployer
 module.exports = async (deployer, network) => {  
 
   
   const accounts      = await web3.eth.getAccounts();
-  // Make feeAccount same as admin account accessible to admins of exchange.
   const feeAccount    = accounts[0];
   const feePercent    = 1;
   
   /*
-  On Rinkeby use rinkeby address for lighthouse
-  On Local make use of Lighthouse.sol 
+  On Rinkeby use rinkeby address
   */
-  // Seperate rinkeby network deployment settings from local development testing deploy setttings
   if(network == "rinkeby") {
 
-    var address = '0x613D2159db9ca2fBB15670286900aD6c1C79cC9a';   //address of RNG lighthouse on rinkeby
+    var address = '0x613D2159db9ca2fBB15670286900aD6c1C79cC9a';   //address of RNG on rinkeby
     await deployer.deploy(Exchange, feeAccount, feePercent, address);
 
-  } else {    // For local testing
+  } else {    // For testing
 
-    // First deploy the lighthouse, then use the lighthouse's address to deploy Exchange. This allows
-    // Exchange to know which lighthouse to obtain data from.
     await deployer.deploy(Lighthouse);
     await deployer.deploy(Exchange, feeAccount, feePercent, Lighthouse.address);
 
